b02a82905376255c245f7c12cf55f215c77ad1dd
Rodrigo Martínez Pérez <rmartinez@cpalianza.com.mx>
2024-07-30 17:02:05 -0600

    Add PhotosEndpointsTests

---
+++ src/Tests/Endpoints/PhotosEndpointsTests.cs
@@ -0,0 +1,70 @@
+﻿using JsonPlaceholderClient.API;
+using System.Net;
+
+namespace JsonPlaceholderClient.Tests.Endpoints;
+
+public sealed class PhotosEndpointsTests : IClassFixture<CustomWebApplicationFactory<AssemblyEntryPoint>>
+{
+    private readonly HttpClient _Client;
+
+    public PhotosEndpointsTests(CustomWebApplicationFactory<AssemblyEntryPoint> ApplicationFactory)
+    {
+        _Client = ApplicationFactory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetAll_Returns200()
+    {
+        const string ExpectedContent = """
+        [{"id":1,"albumId":1,"url":"https://httpstat.us/200","title":"Fake Photo #1","thumbnailUrl":"https://httpstat.us/200"},{"id":2,"albumId":1,"url":"https://httpstat.us/200","title":"Fake Photo #2","thumbnailUrl":"https://httpstat.us/200"},{"id":3,"albumId":1,"url":"https://httpstat.us/200","title":"Fake Photo #3","thumbnailUrl":"https://httpstat.us/200"},{"id":4,"albumId":1,"url":"https://httpstat.us/200","title":"Fake Photo #4","thumbnailUrl":"https://httpstat.us/200"},{"id":5,"albumId":1,"url":"https://httpstat.us/200","title":"Fake Photo #5","thumbnailUrl":"https://httpstat.us/200"}]
+        """;
+
+        using var Response = await _Client.GetAsync("api/v1/photos");
+        var Content = await Response.Content.ReadAsStringAsync();
+
+        Assert.Equal(HttpStatusCode.OK, Response.StatusCode);
+        Assert.Equal(ExpectedContent, Content);
+    }
+
+    [Fact]
+    public async Task GetById_WhenIdExists_Returns200()
+    {
+        const string ExpectedContent = """
+        {"id":1,"albumId":1,"url":"https://httpstat.us/200","title":"Fake Photo #1","thumbnailUrl":"https://httpstat.us/200"}
+        """;
+
+        using var Response = await _Client.GetAsync("api/v1/photos/1");
+        var Content = await Response.Content.ReadAsStringAsync();
+
+        Assert.Equal(HttpStatusCode.OK, Response.StatusCode);
+        Assert.Equal(ExpectedContent, Content);
+    }
+
+    [Fact]
+    public async Task GetById_WhenIdDoesNotExist_Returns404()
+    {
+        using var Response = await _Client.GetAsync("api/v1/photos/0");
+        Assert.Equal(HttpStatusCode.NotFound, Response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetByAlbumId_WhenAlbumExists_Returns200()
+    {
+        const string ExpectedContent = """
+        [{"id":1,"albumId":1,"url":"https://httpstat.us/200","title":"Fake Photo #1","thumbnailUrl":"https://httpstat.us/200"},{"id":2,"albumId":1,"url":"https://httpstat.us/200","title":"Fake Photo #2","thumbnailUrl":"https://httpstat.us/200"},{"id":3,"albumId":1,"url":"https://httpstat.us/200","title":"Fake Photo #3","thumbnailUrl":"https://httpstat.us/200"},{"id":4,"albumId":1,"url":"https://httpstat.us/200","title":"Fake Photo #4","thumbnailUrl":"https://httpstat.us/200"},{"id":5,"albumId":1,"url":"https://httpstat.us/200","title":"Fake Photo #5","thumbnailUrl":"https://httpstat.us/200"}]
+        """;
+
+        using var Response = await _Client.GetAsync("api/v1/albums/1/photos");
+        var Content = await Response.Content.ReadAsStringAsync();
+
+        Assert.Equal(HttpStatusCode.OK, Response.StatusCode);
+        Assert.Equal(ExpectedContent, Content);
+    }
+
+    [Fact]
+    public async Task GetByAlbumId_WhenAlbumDoesNotExist_Returns404()
+    {
+        using var Response = await _Client.GetAsync("api/v1/albums/0/photos");
+        Assert.Equal(HttpStatusCode.NotFound, Response.StatusCode);
+    }
+}
