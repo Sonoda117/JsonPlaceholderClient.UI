b659cc8398aa41a88dfb839a64982589936cc708
Rodrigo Martínez Pérez <rmartinez@cpalianza.com.mx>
2024-08-01 15:36:18 -0600

    Make NavMenu collapsible

---
+++ src/UI/wwwroot/js/NavMenu.js
@@ -0,0 +1,20 @@
+﻿window.navMenu = {
+    enableCollapse: function () {
+        document.getElementById('nav-menu-collapse').addEventListener('click', collapseNavMenu);
+    }
+};
+function collapseNavMenu() {
+    const navMenu = document.getElementById('nav-menu');
+    const collapseIcon = document.getElementById('nav-menu-collapse');
+
+    if (collapseIcon.classList.contains('bi-text-indent-right')) {
+        collapseIcon.classList.remove('bi-text-indent-right');
+        collapseIcon.classList.add('bi-justify');
+    }
+    else {
+        collapseIcon.classList.remove('bi-justify');
+        collapseIcon.classList.add('bi-text-indent-right');
+    }
+    
+    navMenu.classList.toggle('sidebar-collapse');
+}
