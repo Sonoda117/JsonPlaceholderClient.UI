199102474319c863fe5283248bf011d08ea57e68
Rodrigo Martínez Pérez <rmartinez@cpalianza.com.mx>
2024-08-07 18:00:54 -0600

    Add UserProfile page (Contents)

--- src/UI/Components/Pages/UserProfile.razor.cs
+++ src/UI/Components/Pages/UserProfile.razor.cs
@@ -1,4 +1,6 @@
-﻿using JsonPlaceholderClient.Lib.Todos;
+﻿using JsonPlaceholderClient.Lib.Albums;
+using JsonPlaceholderClient.Lib.Posts;
+using JsonPlaceholderClient.Lib.Todos;
 using JsonPlaceholderClient.Lib.Users;
 using Microsoft.AspNetCore.Components;
 
@@ -11,6 +13,8 @@ public partial class UserProfile : IDisposable
 
     private User? User = null;
     private Todo[]? Todos = null;
+    private Post[]? Posts = null;
+    private Album[]? Albums = null;
     private HttpClient Client = null!;
 
     protected override async Task OnInitializedAsync()
@@ -19,10 +23,14 @@ public partial class UserProfile : IDisposable
 
         var TUser = Client.GetFromJsonAsync<User>($"api/v1/users/{UserId}", CancellationToken);
         var TTodos = Client.GetFromJsonAsync<Todo[]>($"api/v1/users/{UserId}/todos", CancellationToken);
+        var TPosts = Client.GetFromJsonAsync<Post[]>($"api/v1/users/{UserId}/posts", CancellationToken);
+        var TAlbums = Client.GetFromJsonAsync<Album[]>($"api/v1/users/{UserId}/albums", CancellationToken);
 
-        await Task.WhenAll(TUser, TTodos);
+        await Task.WhenAll(TUser, TTodos, TPosts, TAlbums);
 
         User = TUser.Result;
+        Posts = TPosts.Result;
+        Albums = TAlbums.Result;
         Todos = [.. TTodos.Result!.OrderByDescending(T => T.Completed)];
     }
     public void Dispose()
